9ddbc30bfb30f64eb6a62e98a04c3ac8bae1a463
WojciechKonior <78218606+WojciechKonior@users.noreply.github.com>
2021-03-24 14:44:16 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Product1
+{
+  public:
+  
+};
+
+class Builder
+{
+  public:
+  
+};
+
+class ConcreteBuilder1: public Builder
+{
+  public:
+  
+};
+
+class Director
+{
+  public:
+  
+};
+
+void ClientCode(Director& director)
+{
+
+}
+
+
+int main()
+{
+
+  return 0;
+}
